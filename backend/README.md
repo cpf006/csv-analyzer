2de270da4fd5bc5cac731b024e80dfde56c5fa33
Connor Ford <connor.ford@theleadingpath.com>
2023-10-12 07:26:21 -0400

    Updating cors and readme

--- backend/README.md
+++ backend/README.md
@@ -4,5 +4,9 @@ pip install requirements.txt
 # Run Server
 uvicorn app.main:app --reload
 
+(You should see the app running at http://127.0.0.1:8000)
+
 # Run Tests
 pytest app/tests/test_main.py
+
+
--- backend/app/main.py
+++ backend/app/main.py
@@ -1,8 +1,22 @@
 from fastapi import FastAPI, UploadFile, File, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
 from .models import CSVAnalysisResponse
 from .utils import process_csv
 
 app = FastAPI()
+
+origins = [
+    "http://localhost:3000",  # React frontend (Should be updated for production to be stored in env)
+    "http://127.0.0.1:3000"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.post("/uploadcsv/", response_model=CSVAnalysisResponse)
 async def upload_csv(file: UploadFile = File(...)):
