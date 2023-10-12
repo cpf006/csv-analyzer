576ef9c217c21960d72f08dcbe8962a5f6e50b01
Connor Ford <connor.ford@theleadingpath.com>
2023-10-12 11:05:00 -0400

    Updating testing

--- frontend/babel.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = (api) => {
-  // Check if we are in test mode
-  if (api.env('test')) {
-    return {
-      "presets": ["next/babel", "@babel/preset-react"]
-    };
-  }
-  return {};
-};
